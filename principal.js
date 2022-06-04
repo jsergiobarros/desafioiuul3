67c695c3eee78e5f3c77ee2baf1e35d48dc886f0
Victor <victorvidigal@gmail.com>
2022-06-04 15:29:23 -0300

    Remoção arquivos .js

--- principal.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var pessoa_1 = require("./pessoa");
-var pessoa = new pessoa_1.Pessoa();
-pessoa.nome = "Victor Vidigal Ribeiro";
-pessoa.idade = 36;
-console.log(pessoa.nome);
-console.log(pessoa.idade);
